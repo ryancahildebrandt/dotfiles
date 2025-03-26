98246b939a24427b1b3a189db1b07314614193f6
ryancahildebrandt <ryancahildebrandt@gmail.com>
2025-03-25 20:26:29 -0400

    helix spacemacs golint

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,28 +1,29 @@
 # ~/.config/fish/config.fish
 
 # Fish options 
+zoxide init --cmd j fish | source
 starship init fish | source
 navi widget fish | source
-fish_add_path ~/miniconda3/bin ~/julia/bin ~/.juliaup/bin ~/.local/bin ~/.local/share ~/.cargo/bin ~/exercism/bin ~/.local/bin/qobuz-dl /root/.cargo/bin
-set -U fish_greeting "
-   °
-    O     /`·.¸
-  0      /¸...¸`:·
-   o ¸.·´  ¸   `·.¸.·´)
-    : © ):´;      ¸  {
-     `·.¸ `·  ¸.·´\`·¸)
-         `\\´´\¸.·´
-
-〜　フ　ィ　ッ　シ　ュ　タ　イ　ム　〜
-
-"
+fish_add_path ~/miniconda3/bin ~/julia/bin ~/.juliaup/bin ~/.local/bin ~/.local/share ~/.cargo/bin ~/exercism/bin ~/.local/bin/qobuz-dl /root/.cargo/bin ~/go/bin ~/helix /usr/bin/ruby
+set -U fish_greeting ""
+# °
+# O     /`·.¸
+# 0      /¸...¸`:·
+# o ¸.·´  ¸   `·.¸.·´)
+# : © ):´;      ¸  {
+# `·.¸ `·  ¸.·´\`·¸)
+# `\\´´\¸.·´
+# 
+# 〜　フ　ィ　ッ　シ　ュ　タ　イ　ム　〜
+# 
+# "
 
 # Custom aliases
-alias lx="ls -1AFhs --color --group-directories-first" 
+alias lx="ls -1AFhs --color --group-directories-first"
 alias ee="exa -GlFa --icons --group-directories-first"
-alias bat="batcat" 
-alias gs="git status" 
-alias sf="source ~/.config/fish/config.fish" 
+alias bat="batcat"
+alias gs="git status"
+alias sf="source ~/.config/fish/config.fish"
 alias gss="find . -name .git -type d -execdir sh -c 'pwd ;git status; echo "------------"; echo' \;"
 alias gsp="find . -name .git -type d -execdir sh -c 'pwd ;git pull; echo "------------"; echo' \;"
 alias cbx="cbonsai -li --time .1 --life 50 --leaf {あ,い,う,え,お}"
@@ -30,7 +31,6 @@ alias jh="cd ~"
 alias tx="tmux"
 alias m="micro"
 alias xx="exit"
-#alias tt="tldr"
 alias nv="navi"
 alias nvp="navi --print"
 alias duck="~/duckdb"
@@ -39,27 +39,24 @@ alias ssp="systemctl --user stop"
 alias ssr="systemctl --user restart"
 alias sss="systemctl --user status"
 alias cpruff="cp ~/ruff.toml ./ruff.toml"
+alias cpgoci="cp ~/.golangci.yml ./.golangci.yml"
 alias rf="ruff format"
 alias rc="ruff check"
-alias neofetch="rsftch"
+alias neofetch="macchina"
 alias ff="fzf"
 alias fd="fdfind"
+alias y="yazi"
+alias uva="source .venv/bin/activate.fish"
+alias uvd="deactivate"
+alias v="nvim"
+alias h="hx"
+alias e="emacs"
 
 # thefuck
 eval (thefuck --alias fuck | tr '\n' ';')
-
-# >>> conda initialize >>> 
-# !! Contents within this block are managed by 'conda init' !! 
-eval /home/ryan/miniconda3/bin/conda "shell.fish" "hook" $argv | source 
-# <<< conda initialize <<< 
-conda deactivate
-
-#autojump
-[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 
 #external commands/config
 source ~/.config/fish/private_commands.fish
 source ~/.config/nnn/nnn_conf.fish
 source ~/.config/fzf/fzf_conf.fish
 source ~/github/dotfiles/blanks/commands.fish
-bash /usr/share/autojump/autojump.sh
