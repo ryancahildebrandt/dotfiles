78509d3c4128deac2e9afc46cfb3bf1b983a0b61
dotstate <dotstate@localhost>
2026-03-15 17:15:12 -0400

    Update 1 file: make_symlinks.sh

--- desktop/make_symlinks.sh
+++ desktop/make_symlinks.sh
@@ -4,5 +4,4 @@ ln -s /home/ryan/HDD/Documents ./Documents
 ln -s /home/ryan/HDD/Music ./Music
 ln -s /home/ryan/HDD/Pictures ./Pictures
 ln -s /home/ryan/HDD/Videos ./Videos
-ln -s /home/ryan/HDD/github ./github
 ln -s /home/ryan/HDD/Qobuz\ Downloads ./Qobuz\ Downloads
