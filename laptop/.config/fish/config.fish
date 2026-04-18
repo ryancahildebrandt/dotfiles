80287e69bae16c6cbe123245bd0b3687709ed302
ryancahildebrandt <ryancahildebrandt@gmail.com>
2026-04-18 01:24:28 +0000

    Update 1 file: config.fish

--- laptop/.config/fish/config.fish
+++ laptop/.config/fish/config.fish
@@ -14,7 +14,6 @@ alias sf="source ~/.config/fish/config.fish"
 alias gs="git status"
 alias gss="find . -name .git -type d -execdir sh -c 'pwd ;git status; echo "------------"; echo' \;"
 alias gsp="find . -name .git -type d -execdir sh -c 'pwd ;git pull; echo "------------"; echo' \;"
-alias cbx="cbonsai -li --time .1 --life 50 --leaf {あ,い,う,え,お}"
 alias jh="cd ~"
 alias tx="tmux"
 alias m="micro"
@@ -24,12 +23,8 @@ alias nv="navi"
 alias duck="~/duckdb"
 alias ff="fzf"
 alias y="yazi"
-alias nxu="sudo nix-channel --update"
-alias nxs="sudo nixos-rebuild --upgrade switch"
-alias nxb="sudo nixos-rebuild --upgrade boot"
-alias nxt="sudo nixos-rebuild --upgrade test"
-alias nxd="sudo nix-collect-garbage -d"
+alias vpnconnect="sudo openvpn --config /etc/openvpn/client2.conf -d"
 
 #external commands/config
-source ~/.config/fish/private_commands.fish
 source ~/github/dotfiles/laptop/blanks/commands.fish
+
